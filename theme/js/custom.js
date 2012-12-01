f25c517f9ef42f867d99e29dd01a17ef1daee932
Kyle Fuller <inbox@kylefuller.co.uk>
2012-12-01 12:37:17 +0000

    Update documentation

---
+++ theme/js/custom.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    function resizeText(){
+        var spanHeight = $('#teaser span').height();
+        var headerHeight = $('#teaser h1').height();
+
+        if (spanHeight < 105 && headerHeight < 210) {
+            $('#teaser span').css({"font-size" : ((spanHeight/73) * 34), "margin-bottom" : ((spanHeight/150)*105)});
+            $('#teaser h1').css({"font-size" : ((headerHeight/52) * 46)});
+        }
+
+        if ($('body').width() < 980) {
+            $('#overlay').css({"display" : "block"});
+        }
+        else {
+            $('#overlay').css({"display" : "none"});
+        }
+    }
+
+    $(window).resize(function(){
+        resizeText();
+    });
+
+    resizeText();
+
+    $('#chooser li').mouseenter(function(e) {
+        var image = $(this).attr('data-image');   
+
+        if (image !== $('#chooser-image').attr('src')) {
+            $('#chooser-image').fadeOut(function() {
+                $(this).attr('src', image);
+                $(this).fadeIn();
+            });
+        }
+    });
+});
