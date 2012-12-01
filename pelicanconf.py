426c1841c97f3f4060ce79e0840bb0d0f56953de
Kyle Fuller <inbox@kylefuller.co.uk>
2012-12-01 12:30:34 +0000

    Support apple-itunes-app meta tag

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,7 +12,8 @@ DEFAULT_LANG = 'en'
 
 DIRECT_TEMPLATES = ('index', 'roadmap',)
 
-PALAVER_APP_STORE_URL = 'http://itunes.apple.com/us/app/palaver/id538073623?ls=1&mt=8'
+APP_STORE_ID = '538073623'
+PALAVER_APP_STORE_URL = 'http://itunes.apple.com/us/app/palaver/id{}?ls=1&mt=8'.format(APP_STORE_ID)
 
 
 DEFAULT_PAGINATION = False
